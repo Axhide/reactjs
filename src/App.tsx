863283c0d97c8fe255884daef629d65a3621dce6
Axhide <borovoy4312@gmail.com>
2023-10-11 12:36:36 +0300

    Lesson props

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import {Rating} from "./components/Rating/Rating";
 
 function App() {
+    console.log("App rendering")
     return (
         <div>
-            This is APP component
-            <p>ssss</p>
-            <div>aaa</div>
+            <AppTitle/>
+            <Rating value ={3}/>
+            <Accordion/>
+            <Rating value ={4}/>
+            <Rating value ={1}/>
+            <Rating value ={2}/>
+            <Rating value ={5}/>
         </div>
     );
+}
+
+function AppTitle () {
+    console.log("AppTitle rendering")
+    return <div>This is APP component</div>
 }
 
 export default App;
